a30fae2c20fab5a1b57e7087d828cf93eaf2fed5
Leila Peltier <l_peltier@outlook.com>
2024-02-29 19:01:43 +0000

    created DTOs

---
+++ my_code/src/main/java/com/example/airline_api/models/FlightDTO.java
@@ -0,0 +1,81 @@
+package com.example.airline_api.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlightDTO {
+    private long id;
+
+    private String destination;
+
+    private int capacity;
+
+    private String departureDate;
+
+    private String departureTime;
+
+    private List<Long> passengerIds;
+
+    public FlightDTO(String destination, int capacity, String departureDate, String departureTime, List<Long> passengerIds) {
+        this.destination = destination;
+        this.capacity = capacity;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+        this.passengerIds = passengerIds;
+    }
+
+    public FlightDTO() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public List<Long> getPassengerIds() {
+        return this.passengerIds;
+    }
+
+    public void setPassengerIds(List<Long> passengerIds) {
+        this.passengerIds = passengerIds;
+    }
+}
+
---
+++ my_code/src/main/java/com/example/airline_api/models/PassengerDTO.java
@@ -0,0 +1,55 @@
+package com.example.airline_api.models;
+
+import java.util.List;
+
+public class PassengerDTO {
+        private long id;
+
+        private String name;
+
+        private String email;
+
+        private List<Long> flightIds;
+
+        public PassengerDTO(String name, String email, List<Long> flightIds) {
+            this.name = name;
+            this.email = email;
+            this.flightIds = flightIds;
+        }
+
+        public PassengerDTO() {
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+    public List<Long> getFlightIds() {
+        return this.flightIds;
+    }
+
+    public void setFlightIds(List<Long> flightIds) {
+        this.flightIds = flightIds;
+    }
+}
+
